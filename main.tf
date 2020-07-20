5cdf1b89dffe7a55354738d424c1806e8699049d
Manjunathan Rajan <32861102+ManjunathanRajan@users.noreply.github.com>
2020-07-21 04:20:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "hashicrop-lear"
+    organization = "hashicrop-learn"
 
     workspaces {
       name = "learn-terraform-circleci"
